846879bb8dac8b7328c289152a51c0cb5ddbc2d0
Harry MacCode <emarh.harry.code@gmail.com>
2024-09-10 13:46:52 +0200

    Navbar component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import './App.css';
+import Card from './components/Card';
+import Nav from './components/Nav';
 
 const App = () => {
   return (
     <div className="App">
       <div class="col-md-6 offset-3 card text-center mt-5">
-        <div class="card-header">
-          <ul class="nav nav-pills card-header-pills">
-            <li class="nav-item">
-              <a class="nav-link active" href="#">Top Stories</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Most Popular</a>
-            </li>
-          </ul>
-        </div>
-        <div class="card-body">
-          <h5 class="card-title">Special title treatment</h5>
-          <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
-        </div>
+        <Nav />
+        <br />
+        <Card />
+        <br />
+        <Card />
       </div>
     </div>
   );
---
+++ src/components/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div class="card-header">
+        <ul class="nav nav-pills card-header-pills">
+        <li class="nav-item">
+            <a class="nav-link active" href="#">Top Stories</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Most Popular</a>
+        </li>
+        </ul>
+    </div>
+  )
+}
+
+export default Nav;
